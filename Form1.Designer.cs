4a0a36db8fa591623a4e950094f6c64894de514f
SPC <SPC@28-LAB402>
2025-03-07 09:37:05 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TinhCanChiNam
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label2 = new Label();
+            list = new ListBox();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(308, 31);
+            label2.Name = "label2";
+            label2.Size = new Size(113, 25);
+            label2.TabIndex = 0;
+            label2.Text = "CanChiNam";
+            // 
+            // list
+            // 
+            list.FormattingEnabled = true;
+            list.ItemHeight = 15;
+            list.Location = new Point(189, 104);
+            list.Name = "list";
+            list.Size = new Size(284, 244);
+            list.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(list);
+            Controls.Add(label2);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label2;
+        private ListBox list;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,19 @@
+﻿namespace TinhCanChiNam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] canArr = { "Giáp", "Ất", "Bính", "Đinh", "Mậu", "Kỷ", "Canh", "Tân", "Nhâm", "Quý" };
+            string[] chiArr = { "Tý", "Sửu", "Dần", "Mão", "Thìn", " Tỵ", "Ngọ", "Mùi", "Thân", "Dậu", "Tuất", "Hợi" };
+            for (int i = 1900; i < 3000; i++) { list.Items.Add(i + ":" + canArr[i % 10] + " " + chiArr[i % 12]); }
+        }
+    }
+}
